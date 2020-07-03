e7a283c543c768953caa19c13becf7923155a7b8
swathi1710 <67016071+swathi1710@users.noreply.github.com>
2020-07-03 15:36:19 +0530

    edited

--- script.js
+++ script.js
@@ -41,9 +41,15 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for( var i=0 ;i < names.length; i++) {
   var firstLetter = names[i].charAt(0);
   if (firstLetter == "j" || firstLetter == "J") {
-    console.log(speakWords +" " + names[i]);
-  } else {
-    console.log( speakWord + " " + names[i])  
+    document.write(speakWords +" " + names[i]);
+    document.write("<br>");
+  
+  }
+   else
+    {
+  document.write( speakWord + " " + names[i]); 
+  document.write("<br>");
+  
 }
 }
 
